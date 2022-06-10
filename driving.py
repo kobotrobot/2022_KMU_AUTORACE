bcd032b952089365152701ccc4161c718ca3e7ec
gyeongseoMin <67200721+gyeongseoMin@users.noreply.github.com>
2022-06-10 16:11:29 +0900

    Update driving.py

--- driving.py
+++ driving.py
@@ -105,7 +105,8 @@ def start():
     # "이미지처리 +차선위치찾기 +조향각결정 +모터토픽발행"
     # 작업을 반복적으로 수행함.
     # =========================================
-    tmp = []
+    tmp1 = []
+    tmp2 = []
     while not rospy.is_shutdown():
         # 이미지처리를 위해 카메라 원본이미지를 img에 복사 저장
         img = image.copy()
@@ -113,68 +114,141 @@ def start():
         whiteLower = (200, 200, 200)
         whiteUpper = (255, 255, 255)
         mask = cv2.inRange(img_rgb, whiteLower, whiteUpper)
+        cv2.imshow('mask', mask)
         img2 = cv2.bitwise_and(img_rgb, img_rgb, mask=mask)
         gray_img = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
         blur_img = cv2.GaussianBlur(gray_img, (3,3), 0)
         dst = cv2.Canny(blur_img, 50, 200)
-        print(dst.shape)
+        #print(dst.shape)
         cv2.imshow('canny', dst)
 
-        # 다각형 roi 지정
+        # 다각형 roi1 지정
         img = np.zeros((480, 640), np.uint8) #height, width
-        print(img.shape)
-        pt = np.array([[45, 360], [0, 430], [640, 430], [595, 360]])
-        cv2.fillConvexPoly(img, pt, (255, 0, 0))
-        print(img.shape)
-        roi = cv2.bitwise_and(dst, img)
-        print(roi.shape)
+        #print(img.shape)
+        roi1_pt = np.array([[45, 360], [0, 430], [310, 430], [310, 360]])
+        cv2.fillConvexPoly(img, roi1_pt, (255, 0, 0))
+        #print(img.shape)
+        roi1 = cv2.bitwise_and(dst, img)
+        #print(roi.shape)
+
+	# roi2 
+        img = np.zeros((480, 640), np.uint8) #height, width
+        #print(img.shape)
+        roi2_pt = np.array([[330, 360], [330, 430], [640, 430], [595, 360]])
+        cv2.fillConvexPoly(img, roi2_pt, (255, 0, 0))
+        #print(img.shape)
+        roi2 = cv2.bitwise_and(dst, img)
+        #print(roi.shape)
 
         # Hough 변환(직선 검출)
         
-        lines = cv2.HoughLinesP(roi, 1.0, np.pi / 180, 50, minLineLength=50, maxLineGap=500)
-        tmp.append(lines)
-        print(tmp)
-        print("------")
-        print(type(lines))
-        print(lines)
+        lines = cv2.HoughLinesP(roi1, 1.0, np.pi / 180, 50, minLineLength=1, maxLineGap=1000)
+        tmp1.append(lines)
+        #print(tmp1)
+        #print("------")
+        #print(type(lines))
+        #print(lines)
         
         if lines is not None:
-            print(lines)
-            pt1 = (lines[0][0][0], lines[0][0][1])
-            pt2 = (lines[0][0][2], lines[0][0][3])
-            cv2.line(image, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
+            #print(lines)
+            roi1_pt1 = (lines[0][0][0], lines[0][0][1])
+            roi1_pt2 = (lines[0][0][2], lines[0][0][3])
+            cv2.line(image, roi1_pt1, roi1_pt2, (0, 0, 255), 2, cv2.LINE_AA)
+            print(" circle roi_pt2", roi1_pt2)
+            
         elif lines is None:
-            lines = tmp[-2]
+            lines = tmp1[-2]
             #pt1 = (lines[0][0][0], lines[0][0][1])
             # pt2 = (lines[0][0][2], lines[0][0][3])
             #cv2.line(image, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
         else: 
             print("fail")
+        cv2.circle(image, roi1_pt2, 5, (255, 0, 0), -1) 
+
+        lines2 = cv2.HoughLinesP(roi2, 1.0, np.pi / 180, 50, minLineLength=1, maxLineGap=1000)
+        tmp2.append(lines2)
+       # print(tmp2)
+        #print("------")
+        #print(type(lines))
+        # print(lines)
+        
+        if lines2 is not None:
+            #print(lines)
+            roi2_pt1 = (lines2[0][0][0], lines2[0][0][1])
+            roi2_pt2 = (lines2[0][0][2], lines2[0][0][3])
+            cv2.line(image, roi2_pt1, roi2_pt2, (0, 0, 255), 2, cv2.LINE_AA)
+            print(" circle ro2_pt1", roi2_pt1)
+            # cv2.circle(image, int(lines2[0][0][2], int(lines2[0][0][3]), (255, 0, 0), -1)
+        elif lines2 is None:
+            lines2 = tmp2[-2]
+            #pt1 = (lines[0][0][0], lines[0][0][1])
+            # pt2 = (lines[0][0][2], lines[0][0][3])
+            #cv2.line(image, pt1, pt2, (0, 0, 255), 2, cv2.LINE_AA)
+        else: 
+            print("fail")
+        cv2.circle(image, roi2_pt1, 5, (255, 0, 0), -1)
 
         # cv2.LINE_AA 뭔지 모름
         cv2.line(image, (0, 360), (640, 360), (0, 255, 0), 1, cv2.LINE_4)
 	#print(2)
         # 좌표 그리기
-        cv2.circle(image, (105, 360), 5, (0, 255, 255), -1)
-        cv2.circle(image, (535, 360), 5, (0, 255, 255), -1)
+        # rec
+
+        # cv2.circle(image, (105, 360), 5, (0, 255, 255), -1)
+        # cv2.circle(image, (535, 360), 5, (0, 255, 255), -1)
 	#print(3)
 
         # 가운데 구하기
-
+        if lines2 is not None and lines is not None:
+            x_pt = (lines2[0][0][0] + lines[0][0][2]) / 2 - 320
+            y = 180
+            angle = atan2(y,x_pt)
+            print("x_pt", x_pt)
+            #cv2.circle(image, (int(x_pt), y), (255, 255, 0), -1)
+            speed = 25
+            if x_pt <=0:
+                angle = -angle
+        elif lines2 is None and lines is not None:
+            lines2 = tmp2[-2]
+            x_pt = 320 - lines[0][0][2]
+            y = 60
+            angle = 32*atan2(y,x_pt)
+            speed = 10
+            if x_pt <= 0:
+                angle = -angle
+        elif lines2 is not None and lines is None:
+            lines = tmp1[-2]
+            x_pt = lines2[0][0][0] - 320
+            
+            y = 60
+            angle = -32*atan2(y,x_pt)
+            speed = 10
+            
+            if x_pt <= 0:
+                angle = -angle
+        else:
+            angle = 0
+            speed = 10
+        print(angle)
+        print(x_pt)
+        
+        ## cv2.circle(image, (x_pt+320, 360), 5, (255, 0, 0), -1)
+	
         # 조향각 구하기(-50-~ +50)
          # left gradient 그리기
-        gradient = float((lines[0][0][1] - lines[0][0][3])) / float((lines[0][0][0] - lines[0][0][2]))
-        if gradient < 0:
-             left_grad = gradient 
+        #gradient = float((lines[0][0][1] - lines[0][0][3])) / float((lines[0][0][0] - lines[0][0][2]))
+        #if gradient < 0:
+        #     left_grad = gradient 
         #left_grad = float((lines[0][0][1] - lines[0][0][3])) / float((lines[0][0][0] - lines[0][0][2]))
-             print(left_grad)
-        ele: gradient > 0:
-             right_grad = gradient
-             print(right_grad)
+        #     print(left_grad)
+        #else: gradient > 0:
+        #     right_grad = gradient
+             
        
         #right gradient 그리기
         # right_grad = float((lines[0][0][1] - lines[0][0][3])) / float((lines[0][0][0] - lines[0][0][2]))
         
+        #if 
         # n2 = float(lines[0][0][1] - (right_grad * lines[0][0][0]))
 
 
@@ -184,7 +258,7 @@ def start():
         # =========================================
 
         # 우선 테스트를 위해 직진(0값)으로 설정
-        # angle = 0
+        #angle = 0
         # print(angle())
 
         # =========================================
@@ -194,15 +268,16 @@ def start():
         # =========================================
 
         # 우선 테스트를 위해 느린속도(10값)로 설정
-        speed = 20
+        #speed = 20
         # 디버깅을 위해 모니터에 이미지를 디스플레이
         cv2.imshow("CAM View", img)
-        cv2.imshow("roi", roi)
+        cv2.imshow("roi1", roi1)
+        cv2.imshow('roi2', roi2)
         cv2.imshow('+', image)
         cv2.waitKey(1)
 
         # drive() 호출. drive()함수 안에서 모터 토픽이 발행됨.
-        drive(angle(), speed)
+        drive(angle, speed)
 
 # =============================================
 # 메인 함수
